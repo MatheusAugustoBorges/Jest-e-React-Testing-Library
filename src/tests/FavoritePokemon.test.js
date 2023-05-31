9990b17fc39f9265a52c82f0848b02cccb0fb1f7
Matheus Augusto <matheus_augusto92@hotmail.com>
2023-05-31 18:23:39 -0300

    requirement 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,78 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+// import { act } from 'react-dom/test-utils';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+
+const pokemonList = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://archives.bulbagarden.net/media/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://archives.bulbagarden.net/media/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://archives.bulbagarden.net/media/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.',
+  },
+  {
+    id: 4,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://archives.bulbagarden.net/media/upload/0/0a/Spr_5b_004.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Alola Route 3',
+        map: 'https://archives.bulbagarden.net/media/upload/9/93/Alola_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 3',
+        map: 'https://archives.bulbagarden.net/media/upload/4/4a/Kanto_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 4',
+        map: 'https://archives.bulbagarden.net/media/upload/2/24/Kanto_Route_4_Map.png',
+      },
+      {
+        location: 'Kanto Rock Tunnel',
+        map: 'https://archives.bulbagarden.net/media/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+      },
+    ],
+    summary: 'The flame on its tail shows the strength of its life force. If it is weak, the flame also burns weakly.',
+  },
+];
+
+describe('<FavoritePokemon.js />', () => {
+  it('Verificar se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+    renderWithRouter(<FavoritePokemon pokemonList={ [] } />);
+
+    expect(screen.getByText('No favorite Pokémon found')).toBeInTheDocument();
+  });
+
+  it('Verificar se apenas são exibidos os Pokémon favoritados', () => {
+    renderWithRouter(<FavoritePokemon pokemonList={ pokemonList } />);
+
+    expect(screen.getByText(`${pokemonList[0].name}`)).toBeInTheDocument();
+    expect(screen.getByText(`${pokemonList[0].type}`)).toBeInTheDocument();
+
+    expect(screen.getByText(`${pokemonList[1].name}`)).toBeInTheDocument();
+    expect(screen.getByText(`${pokemonList[1].type}`)).toBeInTheDocument();
+  });
+});
