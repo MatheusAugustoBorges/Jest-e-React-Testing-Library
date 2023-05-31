3f2d0613151c171ff40fad81e042aa23627d2ee1
Matheus Augusto <matheus_augusto92@hotmail.com>
2023-05-31 18:35:02 -0300

    requirement 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('<NotFound.js />', () => {
+  it('Verifica se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    // Verifica título na NotFound com tag <h2 />
+    const titlePageNotFound = screen.getByRole('heading', { level: 2 });
+
+    // Verifica título na NotFound com tag <h2 /> sendo 'Page requested not found'
+    expect(titlePageNotFound).toHaveTextContent('Page requested not found');
+  });
+
+  it('Verifica se a página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    // Verifica a imagem na página através da tag <img />
+    const image = screen.getByRole('img');
+
+    // Verifica o atributo src da imagem - se a URL está correta
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
