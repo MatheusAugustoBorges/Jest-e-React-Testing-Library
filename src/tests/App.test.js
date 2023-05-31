7d0f59dd7b02ba7c7552071faa494d59ce4c8f45
Matheus Augusto <matheus_augusto92@hotmail.com>
2023-05-31 16:00:30 -0300

    requirement 1 - v2.0

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,63 +4,69 @@ import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
-test('Verifica se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
-  const { history } = renderWithRouter(<App />);
+describe('<App /> - Verifica se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  it('Verfica se a aplicação é redirecionada para a página inicial, na URL / ao clicar no link Home da barra de navegação;', () => {
+    const { history } = renderWithRouter(<App />);
 
-  // ------------------------Link Home---------------------------------
-  // Verifica link Home no <App />
-  const homeLink = screen.getByRole('link', { name: /Home/i });
-  expect(homeLink).toBeInTheDocument();
+    // ------------------------Link Home---------------------------------
+    // Verifica link Home no <App />
+    const homeLink = screen.getByRole('link', { name: /Home/i });
+    expect(homeLink).toBeInTheDocument();
 
-  // Clica no link "Home"
-  act(() => {
-    userEvent.click(homeLink);
+    // Clica no link "Home"
+    act(() => {
+      userEvent.click(homeLink);
+    });
+
+    // Verifica rota da Home ('/')
+    expect(history.location.pathname).toBe('/');
   });
-
-  // Verifica título na Home com tag <h2 />
-  const titlePageHome = screen.getByRole('heading', { level: 2 });
-
-  // Verifica rota da Home ('/')
-  expect(history.location.pathname).toBe('/');
-
-  // Verifica título na Home com tag <h2 /> sendo 'Encountered Pokémon'
-  expect(titlePageHome).toHaveTextContent('Encountered Pokémon');
-
   // ------------------------Link About---------------------------------
-  // Verifica link About no <App />
-  const aboutLink = screen.getByRole('link', { name: /About/i });
-  expect(aboutLink).toBeInTheDocument();
+  it('Verfica se a aplicação é redirecionada para a página de About, na URL /about, ao clicar no link About da barra de navegação', () => {
+    const { history } = renderWithRouter(<App />);
 
-  // Clica no link "About"
-  act(() => {
-    userEvent.click(aboutLink);
+    // Verifica link About no <App />
+    const aboutLink = screen.getByRole('link', { name: /About/i });
+    expect(aboutLink).toBeInTheDocument();
+
+    // Clica no link "About"
+    act(() => {
+      userEvent.click(aboutLink);
+    });
+
+    // Verifica rota da About ('/about')
+    expect(history.location.pathname).toBe('/about');
   });
 
-  // Verifica título na About com tag <h2 />
-  const titlePageAbout = screen.getByRole('heading', { level: 2 });
+  // ------------------------Link Favorite Pokémon---------------------------------
+  it('Verfica se a aplicação é redirecionada para a página de Pokémon Favoritados, na URL /favorites, ao clicar no link Favorite Pokémon da barra de navegação', () => {
+    const { history } = renderWithRouter(<App />);
 
-  // Verifica rota da About ('/about')
-  expect(history.location.pathname).toBe('/about');
+    // Verifica link Favorite Pokémon no <App />
+    const favoritePokemonLink = screen.getByRole('link', { name: /Favorite Pokémon/i });
+    expect(favoritePokemonLink).toBeInTheDocument();
 
-  // Verifica título na About com tag <h2 /> sendo 'About Pokédex'
-  expect(titlePageAbout).toHaveTextContent('About Pokédex');
+    // Clica no link "Favorite Pokémon"
+    act(() => {
+      userEvent.click(favoritePokemonLink);
+    });
 
-  // ------------------------Link About---------------------------------
-  // Verifica link Favorite Pokémon no <App />
-  const favoritePokemonLink = screen.getByRole('link', { name: /Favorite Pokémon/i });
-  expect(favoritePokemonLink).toBeInTheDocument();
-
-  // Clica no link "Favorite Pokémon"
-  act(() => {
-    userEvent.click(favoritePokemonLink);
+    // Verifica rota da Favorite Pokémon ('/favorites')
+    expect(history.location.pathname).toBe('/favorites');
   });
 
-  // Verifica título na About com tag <h2 />
-  const titlePageFavoritePokemon = screen.getByRole('heading', { level: 2 });
+  // ------------------------Link Inexistente---------------------------------
+  it('Verfica se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida.', () => {
+    const { history } = renderWithRouter(<App />);
 
-  // Verifica rota da About ('/about')
-  expect(history.location.pathname).toBe('/favorites');
+    // Verifica o link inexistente
+    act(() => {
+      history.push('/semnome');
+    });
 
-  // Verifica título na About com tag <h2 /> sendo 'About Pokédex'
-  expect(titlePageFavoritePokemon).toHaveTextContent('Favorite Pokémon');
+    const titleNotFound = screen.getByRole('heading', { level: 2 });
+
+    expect(history.location.pathname).toBe('/semnome');
+    expect(titleNotFound).toHaveTextContent('Page requested not found');
+  });
 });
