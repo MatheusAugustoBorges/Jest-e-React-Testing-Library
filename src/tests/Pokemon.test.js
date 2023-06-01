e3809aee530564f648f1c206f8e793a94eaa87a5
Matheus Augusto <matheus_augusto92@hotmail.com>
2023-06-01 18:22:32 -0300

    requirement 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -27,36 +27,6 @@ const pokemonListSimulator = [
     ],
     summary: 'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.',
   },
-  {
-    id: 4,
-    name: 'Charmander',
-    type: 'Fire',
-    averageWeight: {
-      value: '8.5',
-      measurementUnit: 'kg',
-    },
-    image: 'https://archives.bulbagarden.net/media/upload/0/0a/Spr_5b_004.png',
-    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
-    foundAt: [
-      {
-        location: 'Alola Route 3',
-        map: 'https://archives.bulbagarden.net/media/upload/9/93/Alola_Route_3_Map.png',
-      },
-      {
-        location: 'Kanto Route 3',
-        map: 'https://archives.bulbagarden.net/media/upload/4/4a/Kanto_Route_3_Map.png',
-      },
-      {
-        location: 'Kanto Route 4',
-        map: 'https://archives.bulbagarden.net/media/upload/2/24/Kanto_Route_4_Map.png',
-      },
-      {
-        location: 'Kanto Rock Tunnel',
-        map: 'https://archives.bulbagarden.net/media/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
-      },
-    ],
-    summary: 'The flame on its tail shows the strength of its life force. If it is weak, the flame also burns weakly.',
-  },
 ];
 
 const isPokemonFavoriteById = {
@@ -83,20 +53,9 @@ describe('<Pokemon />', () => {
     // Verifica nome do Pikachu
     expect(screen.getByText(`${pokemonListSimulator[0].name}`)).toBeInTheDocument();
 
-    // ------- Outra forma de capturar o tipo na aplicação -------
-    // Verifica tipo do Pikachu
-    // const typePikachu = screen.queryByTestId('pokemon-type');
-    // expect(typePikachu).toBeInTheDocument();
-    // -------------------------------------------------------
-
     // Verifica tipo do Pikachu
     const typePikachu = screen.queryAllByText(`${pokemonListSimulator[0].type}`);
-    expect(typePikachu[0]).toBeInTheDocument();
-
-    // ------- Tentativa de correção -------
-    // pokemonListSimulator[1].type = '';
-    // const typeErrorPikachu = screen.queryAllByText(`${pokemonListSimulator[1].type}`);
-    // expect(typeErrorPikachu[0]).not.toBeInTheDocument();
+    expect(typePikachu).toHaveLength(2);
     // -------------------------------------------------------
 
     // Verifica peso do Pikachu
